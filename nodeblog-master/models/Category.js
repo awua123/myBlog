3bc21edb4406fb464b065b124b929d58073cbd65
AWUA <1477016531@qq.com>
2021-08-28 20:21:08 +0800

    Delete nodeblog-master directory

--- nodeblog-master/models/Category.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const categoriesSchema = require('../schemas/categories')
-
-// 定义一个模型类
-module.exports = mongoose.model('Category', categoriesSchema)
--- nodeblog-master/models/user.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = require('../schemas/users')
-
-// 定义一个模型类
-module.exports = mongoose.model('User', userSchema)
